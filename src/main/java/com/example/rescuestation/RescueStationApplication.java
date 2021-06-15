fc32c64340f5b5a7f7ef44f74d5f052a899b5bcf
KLSCX <2576723093@qq.com>
2021-06-15 21:01:21 +0800

    init

---
+++ src/main/java/com/example/rescuestation/RescueStationApplication.java
@@ -0,0 +1,13 @@
+package com.example.rescuestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescueStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RescueStationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rescuestation/RescueStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rescuestation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
